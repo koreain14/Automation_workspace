af557cf9a7bacb945351dc8367c7541e1bb506cd
Sam <koreain14@gmail.com>
2017-03-17 14:17:33 +0900

    no message

---
+++ Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕친구야");
+            Console.ReadKey();
+        }
+    }
+}
